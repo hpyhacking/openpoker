d72d40b2fa69c0e2b4836dcf0dcef7e73872bde1
HpyHacking <crazyflapjack@gmail.com>
2012-07-02 10:41:30 +0800

    add webtekcos handle

---
+++ src/handle/genesis_game.erl
@@ -0,0 +1,11 @@
+-module(genesis_game).
+-export([connect/0, disconnect/1, handle_message/2, hanlde_data/2]).
+
+-include("common.hrl").
+-include("game.hrl").
+-include("protocol.hrl").
+
+connect() -> {}.
+disconnect(_) -> {}.
+handle_message(_, _) -> {}.
+hanlde_data(_, _) -> {}.
