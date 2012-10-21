6f9615f2f9562c866738331bbf301cde63e3bb82
HpyHacking <crazyflapjack@gmail.com>
2012-10-21 11:31:39 +0800

    add photo support

--- src/schema.erl
+++ src/schema.erl
@@ -81,7 +81,7 @@ setup_games() ->
   mnesia:dirty_write(#tab_game_config{id = 3, module = game, mods = ?DEF_MOD, limit = Limit#limit{small = 500, big = 1000}, seat_count = 5, start_delay = 3000, required = 2, timeout = 1000, max = 10}).
 
 setup_players() ->
-  mnesia:dirty_write(#tab_player_info{pid = 1, identity = "player", password = ?DEF_HASH_PWD, nick = "nick", photo = "default", cash = 10000}),
-  mnesia:dirty_write(#tab_player_info{pid = 2, identity = "robot", password = ?DEF_HASH_PWD, nick = "robot", photo = "default", cash = 10000}),
-  mnesia:dirty_write(#tab_player_info{pid = 3, identity = "robot_foo", password = ?DEF_HASH_PWD, nick = "robot foo", photo = "default", cash = 10000}),
-  mnesia:dirty_write(#tab_player_info{pid = 4, identity = "robot_doo", password = ?DEF_HASH_PWD, nick = "robot doo", photo = "default", cash = 10000}).
+  mnesia:dirty_write(#tab_player_info{pid = 1, identity = "player", password = ?DEF_HASH_PWD, nick = "nick", photo = "default.png", cash = 10000}),
+  mnesia:dirty_write(#tab_player_info{pid = 2, identity = "robot", password = ?DEF_HASH_PWD, nick = "robot", photo = "2012491443.jpg", cash = 10000}),
+  mnesia:dirty_write(#tab_player_info{pid = 3, identity = "robot_foo", password = ?DEF_HASH_PWD, nick = "robot foo", photo = "2011962129360.jpg", cash = 10000}),
+  mnesia:dirty_write(#tab_player_info{pid = 4, identity = "robot_doo", password = ?DEF_HASH_PWD, nick = "robot doo", photo = "2012491439.jpg", cash = 10000}).
